b1c493fbdb751c477e007a97a4cec2c0870b7bd1
Sidharth S <sidharth@hifx.co.in>
2025-03-25 16:53:59 +0530

    initial commit

---
+++ entity/token.go
@@ -0,0 +1,40 @@
+package entity
+
+type Token struct {
+	Expiry    int64
+	Id        string
+	UserEmail string
+}
+
+type TokenPool []*Token
+
+func (t *TokenPool) Push(x any) {
+	data := x.(Token)
+	*t = append(*t, &data)
+}
+
+func (t *TokenPool) Pop() any {
+	length := len(*t)
+	token := (*t)[length-1]
+	*t = (*t)[:length-1]
+	return token
+}
+
+func (t TokenPool) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t TokenPool) Len() int {
+	return len(t)
+}
+
+func (t TokenPool) Less(i, j int) bool {
+	return t[i].Expiry < t[j].Expiry
+}
+
+func (t *TokenPool) Update(items []Token) {
+	(*t) = (*t)[:0]
+	for _, data := range items {
+		t.Push(data)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sidharth-125/token_heap
+
+go 1.22.1
+
+require github.com/google/uuid v1.6.0
